6bd51dba672daefe637c54796e61edb60603e0f3
Deyverson <deyverson73@gmail.com>
2020-10-13 22:10:42 -0300

    NLW - Second day

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import "express-async-errors";
+import path from "path";
+import cors from "cors";
+import "../src/database/connection";
+import routes from "./routes";
+import errorHandler from "./errors/handle";
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(errorHandler);
+app.listen(3333);
