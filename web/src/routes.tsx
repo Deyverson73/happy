87d62e4107b53b06fdea93478dd8e8b0f8ee707c
Deyverson <deyverson73@gmail.com>
2020-10-14 22:42:27 -0300

    NLW - Third day

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
 import OrphanagesMap from "./pages/OrphanagesMap";
+import Orphanage from "./pages/Orphanage";
+import CreateOrphanages from "./pages/CreateOrphanage";
 
 function Routes() {
   return (
@@ -10,6 +12,8 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/app" component={OrphanagesMap} />
+        <Route path="/orphanages/create" component={CreateOrphanages} />
+        <Route path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
